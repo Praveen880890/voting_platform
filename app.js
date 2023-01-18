07f6e6fd2ac6010a4ab53126cdcc30b566b567ef
Praveen880890 <s.praveenchowdarysureddy@gmail.com>
2023-01-18 19:02:13 +0530

    added login page and some views

---
+++ app.js
@@ -0,0 +1,199 @@
+const express = require("express");
+const app = express();
+const csrf = require("tiny-csrf");
+const cookieParser = require("cookie-parser");
+const { Admin } = require("./models");
+const bodyParser = require("body-parser");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const flash = require("connect-flash");
+const LocalStratergy = require("passport-local");
+const saltRounds = 10;
+app.set("views", path.join(__dirname, "views"));
+app.use(flash());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("Some secret String"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+app.use(
+  session({
+    secret: "my-super-secret-key-2837428907583420",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+app.use((request, response, next) => {
+  response.locals.messages = request.flash();
+  next();
+});
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStratergy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      Admin.findOne({ where: { email: username } })
+        .then(async (user) => {
+          const val = await bcrypt.compare(password, user.password);
+          if (val) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Invalid password" });
+          }
+        })
+        .catch(() => {
+          return done(null, false, { message: "Invalid Email-ID" });
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  Admin.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (request, response) => {
+  if (request.user) {
+    return response.redirect("/election");
+  } else {
+    response.render("index", {
+      title: "Online Voting Platform",
+    });
+  }
+});
+
+app.get(
+  "/election",
+  connectEnsureLogin.ensureLoggedIn(),
+  (request, response) => {
+    response.render("election", {
+      title: "Online Voting Platform",
+    });
+  }
+);
+
+app.get("/signup", (request, response) => {
+  response.render("signup", {
+    title: "Create a new account",
+    csrfToken: request.csrfToken(),
+  });
+});
+
+app.get("/login", (request, response) => {
+  response.render("login", {
+    title: "Login to your account",
+    csrfToken: request.csrfToken(),
+  });
+});
+
+app.get("/signout", (request, response, next) => {
+  request.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    response.redirect("/");
+  });
+});
+
+app.post(
+  "/session",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (request, response) => {
+    response.redirect("/election");
+  }
+);
+
+app.get(
+  "/pswdReset",
+  connectEnsureLogin.ensureLoggedIn(),
+  (request, response) => {
+    response.render("pswdReset", {
+      title: "Reset your password",
+      csrfToken: request.csrfToken(),
+    });
+  }
+);
+
+app.post(
+  "/pswdReset",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    if (!request.body.old_password) {
+      request.flash("error", "Please enter your old password");
+      return response.redirect("/pswdReset");
+    }
+    if (!request.body.new_password) {
+      request.flash("error", "Please enter the new password");
+      return response.redirect("/pswdReset");
+    }
+    if (request.body.new_password.length < 8) {
+      request.flash("error", "Password length should be atleast 8 characters  length");
+      return response.redirect("/pswdReset");
+    }
+    const newPassword = await bcrypt.hash(
+      request.body.new_password,
+      saltRounds
+    );
+    const val = await bcrypt.compare(
+      request.body.old_password,
+      request.user.password
+    );
+    if (val) {
+      Admin.findOne({ where: { email: request.user.email } }).then((user) => {
+        user.resetPass(newPassword);
+      });
+      request.flash("success", "Password changed successfully");
+      return response.redirect("/election");
+    } else {
+      request.flash("error", "Old password does not match");
+      return response.redirect("/pswdReset");
+    }
+  }
+);
+
+app.post("/admin", async (request, response) => {
+  const Pwd = await bcrypt.hash(request.body.password, saltRounds);
+  try {
+    const admin = await Admin.create({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: Pwd,
+    });
+    request.login(admin, (err) => {
+      if (err) {
+        response.redirect("/");
+      } else {
+        response.redirect("/election");
+      }
+    });
+  } catch (error) {
+    request.flash("error", error.message);
+    return response.redirect("/signup");
+  }
+});
+
+module.exports = app;
